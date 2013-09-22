3c3161b8bbad49eefcef930ffef46f148538e4b2
Frederik Hahne <frederik.hahne@gmail.com>
2013-09-22 16:58:14 +0200

    Empty Author Entity Class.

---
+++ bookshelf.domain/src/main/java/de/atomfrede/bookshelf/domain/entity/author/Author.java
@@ -0,0 +1,13 @@
+package de.atomfrede.bookshelf.domain.entity.author;
+
+import de.atomfrede.bookshelf.domain.entity.AbstractEntity;
+
+public class Author extends AbstractEntity {
+
+	@Override
+	public Long getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
