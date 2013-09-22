2cc1940f4102039a47ececad8cb63983512ce690
Frederik Hahne <frederik.hahne@gmail.com>
2013-09-22 17:09:46 +0200

    Fixed Build

--- bookshelf.domain/src/main/java/de/atomfrede/bookshelf/domain/entity/author/Author.java
+++ bookshelf.domain/src/main/java/de/atomfrede/bookshelf/domain/entity/author/Author.java
@@ -1,16 +1,34 @@
 package de.atomfrede.bookshelf.domain.entity.author;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
 
 import de.atomfrede.bookshelf.domain.entity.AbstractEntity;
 
 @Entity
+@Table(name = "author")
 public class Author extends AbstractEntity {
 
+	@GenericGenerator(name = "AuthorIdGenerator", strategy = "org.hibernate.id.MultipleHiLoPerTableGenerator", parameters = {
+			@Parameter(name = "table", value = "IdentityGenerator"),
+			@Parameter(name = "primary_key_column", value = "sequence_name"),
+			@Parameter(name = "primary_key_value", value = "Author"),
+			@Parameter(name = "value_column", value = "next_hi_value"),
+			@Parameter(name = "primary_key_length", value = "100"),
+			@Parameter(name = "max_lo", value = "1000") })
+	@Id
+	@GeneratedValue(strategy = GenerationType.TABLE, generator = "AuthorIdGenerator")
+	private Long id;
+	
 	@Override
 	public Long getId() {
-		// TODO Auto-generated method stub
-		return null;
+		return id;
 	}
 
 }
--- bookshelf.domain/src/main/java/de/atomfrede/bookshelf/domain/entity/user/User.java
+++ bookshelf.domain/src/main/java/de/atomfrede/bookshelf/domain/entity/user/User.java
@@ -1,13 +1,34 @@
 package de.atomfrede.bookshelf.domain.entity.user;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+
 import de.atomfrede.bookshelf.domain.entity.AbstractEntity;
 
-public class User extends AbstractEntity{
+@Entity
+@Table(name = "user")
+public class User extends AbstractEntity {
+
+	@GenericGenerator(name = "UserIdGenerator", strategy = "org.hibernate.id.MultipleHiLoPerTableGenerator", parameters = {
+			@Parameter(name = "table", value = "IdentityGenerator"),
+			@Parameter(name = "primary_key_column", value = "sequence_name"),
+			@Parameter(name = "primary_key_value", value = "User"),
+			@Parameter(name = "value_column", value = "next_hi_value"),
+			@Parameter(name = "primary_key_length", value = "100"),
+			@Parameter(name = "max_lo", value = "1000") })
+	@Id
+	@GeneratedValue(strategy = GenerationType.TABLE, generator = "UserIdGenerator")
+	private Long id;
 
 	@Override
 	public Long getId() {
-		// TODO Auto-generated method stub
-		return null;
+		return id;
 	}
 
 }
